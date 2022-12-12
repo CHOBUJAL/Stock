8182eb7b06cf8fc9a119a867f5e802b5e8583623
boyeon <boyeonning@gmail.com>
2022-12-12 18:00:16 +0900

    feat : stock_trading.py

--- stock_trading.py
+++ stock_trading.py
@@ -1,14 +1,16 @@
 import pandas as pd 
 from stock_data import get_symbol_daily_data
 
-a = get_symbol_daily_data(symbol="000060", since='20100101')
+df = get_symbol_daily_data(symbol="000060", since='20100101')
 
 want_col = ['stck_bsop_date', 'stck_clpr', 'stck_hgpr', 'stck_lwpr', 'stck_oprc', 'acml_vol', 'acml_tr_pbmn']
-df = a[want_col]
+df = df[want_col]
 df.columns = ['date', 'close', 'high', 'low', 'open', 'volume', 'amount']
 df['date'] = pd.to_datetime(df['date'])
-df = df
 
 df.sort_values('date', ascending=True, inplace=True)
 df['ma5'] = df['close'].rolling(window=5).mean()
+df['ma20'] = df['close'].rolling(window=20).mean()
+df['ma60'] = df['close'].rolling(window=60).mean()
+df['ma120'] = df['close'].rolling(window=120).mean()
 print(df)
